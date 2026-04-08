d27c8d013a6030414382f0cabab083cd11a6d9c7
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2026-04-08 20:37:19 +0300

    forgot the comma

--- Casks/widget-porting-toolkit.rb
+++ Casks/widget-porting-toolkit.rb
@@ -2,7 +2,7 @@ cask "widget-porting-toolkit" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/nikolan123/WidgetPortingToolkit/releases/download/nightly/Widget.Porting.Toolkit.zip"
+  url "https://github.com/nikolan123/WidgetPortingToolkit/releases/download/nightly/Widget.Porting.Toolkit.zip",
     verified: "github.com/nikolan123/WidgetPortingToolkit/"
   name "Widget Porting Toolkit"
   desc "Run legacy Dashboard Widgets natively"
