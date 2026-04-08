1cf42ace5eb94da647ae647b89c4010c2da61329
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2026-04-08 20:40:57 +0300

    correct offences one last time hopefully

--- Casks/widget-porting-toolkit.rb
+++ Casks/widget-porting-toolkit.rb
@@ -3,13 +3,13 @@ cask "widget-porting-toolkit" do
   sha256 :no_check
 
   url "https://github.com/nikolan123/WidgetPortingToolkit/releases/download/nightly/Widget.Porting.Toolkit.zip",
-    verified: "github.com/nikolan123/WidgetPortingToolkit/"
+      verified: "github.com/nikolan123/WidgetPortingToolkit/"
   name "Widget Porting Toolkit"
   desc "Run legacy Dashboard Widgets natively"
   homepage "https://wdgt.niko.rocks/"
 
   depends_on macos: ">= :monterey"
-  
+
   app "Widget Porting Toolkit.app"
 
   postflight do
